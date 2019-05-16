8da5ab2f3a65bbe083a40cad9e340c5061d71eb7
Rafal.Krakiewicz <Rafal.Krakiewicz@WAW-JT9NV02.corpnet.liox.org>
2019-05-16 15:37:23 +0200

    comin

--- ProjectNaZaliczenie/src/main/Funkcje.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Funkcje {
-
-	public static void main(String[] args) {
-		
-		Main.getMenu();
-	}
-}
--- ProjectNaZaliczenie/src/main/Main.java
+++ ProjectNaZaliczenie/src/main/Main.java
@@ -2,24 +2,54 @@ package main;
 
 import java.util.Scanner;
 
+
+
 public class Main {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
+
+		welcome();
+		int choice = 0;
 		
-		System.out.println("Wiaj");
-		System.out.println("Oto możliwe wybory");
-		
-		Scanner scan = new Scanner(System.in);		
-		
-		getMenu();
-		
+		boolean exit = false;
+
+		while (!exit) {
+
+			choice = getMenu();
+
+			if (choice == 1) {
+				Variables variables = new Variables();
+			}
+
+			if (choice == 5) {
+				exit = true;
+
+			}
+
+		}
+
 	}
-	
-	public static void getMenu() {
-		
-		String result = "1: zmienne\n";
-		result += "2: typy\n";
-		
-		System.out.println(result);
+
+	private static void welcome() {
+		System.out.println("Języki i Paradygmany Programowania");
+		System.out.println("Projekt na zaliczenie: wybrane abstrakcje dla jezyka JAVA");
+		System.out.println("Rafał Krakiewicz");
+		System.out.println("***********************************************************");
+	}
+
+	public static int getMenu() {
+
+		System.out.println("\nMenu:");
+		System.out.println("1 - zmienne");
+		System.out.println("2 - typy danych");
+		System.out.println("3 - struktury sterujące warunkowe");
+		System.out.println("4 - struktury sterujące iteracyjne");
+		System.out.println("5 - wyjście");
+
+		Scanner scan = new Scanner(System.in);
+
+		String choice = scan.nextLine();
+
+		return Integer.parseInt(choice);
 	}
 }
